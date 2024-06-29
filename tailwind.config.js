135a9a7117e5f679aa5ca2401ed099a721d6d2ee
Masoumeh Mohammadi <ms.mohammadi69@gmail.com>
2024-06-29 17:13:58 +0330

    first

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {
+      width: {
+        '22' : '5.5rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
